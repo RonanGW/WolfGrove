a95d5551ecfeb3af3775ccafa1d3bb41df084624
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-28 15:51:13 -0700

    Added a sticky header with links to sections.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-scroll'
 import './App.css';
 import Intro from './Intro.js';
 import Bio from './Bio.js';
@@ -13,10 +14,18 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1>Wolf Grove</h1>
-                    <h3>Ronan</h3>
+                    <div>
+                        <h1>Wolf Grove</h1>
+                        {/*Icon*/}
+                    </div>
+                    <Link to="Intro" spy={true} smooth={true}>Intro</Link>
+                    <Link to="Bio" spy={true} smooth={true}>Bio</Link>
+                    <Link to="Education" spy={true} smooth={true}>Education</Link>
+                    <Link to="Resume" spy={true} smooth={true}>Resume</Link>
+                    <Link to="Portfolio" spy={true} smooth={true}>Portfolio</Link>
+                    <Link to="Testimonials" spy={true} smooth={true}>Testimonials</Link>
                 </header>
-                <Intro />
+                <Intro/>
                 <Bio />
                 <Education />
                 <Resume />
