05636a9e9052cd8d29786589a491d66bf72e1f6e
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-30 16:29:12 -0700

    Added row-wrapping to stop content from being shoved off the side in very small windows.

--- src/Intro.js
+++ src/Intro.js
@@ -8,7 +8,7 @@ class Intro extends Component {
                 <h1>Welcome!</h1>
                 <h3>Important Note</h3>
                 <p> Welcome to my site. It's in it's infancy at the moment, but I intend to for this to be catch all location for everything me! That means this'll be a big project,
-                    which will encompass a fair bit more than just this page. First at foremost, that's me, to the right. So if you catch any bugs or just have opinions, feel free to
+                    which will encompass a fair bit more than just this page. So if you catch any bugs or just have opinions, feel free to
                     contact me. Please take a look through my page and see if you find anything of interest. I'll be posting projects, life updates, skills and affiliations. If you'd like to know more
                     or have access to anything password-locked, reach out and I'd be happy to help. My contact details with be available throughout this site.
                 </p>
