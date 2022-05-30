d4488f757701df08e268db3fdbc7d5d2ba61dab4
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-29 18:34:19 -0700

    Added to About section a swapping Highlight/Story Feature

--- src/Bio.js
+++ src/Bio.js
@@ -4,6 +4,22 @@ import './App.css';
 
 class Bio extends Component {
 
+    constructor(props) {
+        super(props);
+
+        // Define the initial state:
+        this.state = {
+            highlights: false,
+        };
+    }
+
+    handleFade = () => {
+        // Update our state here...
+        this.setState({
+            highlights: !this.state.highlights
+        })
+    };
+
     DownloadResume = () => {
         saveAs(
             "./RonanWolfe_Resume.pdf",
@@ -27,13 +43,23 @@ class Bio extends Component {
                         <button className="LILogo" onClick={this.Redirect.bind(this, "https://www.linkedin.com/in/ronan-wolfe/")}></button>
                     </div>
                     <img src="pfp.jpg" className="Bio-PFP" />
-                    <p> I have an aspiring software engineer with the credentials to back up that dream. I grew up in Los Angeles where I took an interest in
-                        the life sciences, working as a zookeeper and frequently volunteering for conservation projects. Since 2018 I have traveled
-                        around the country and gone to college. I've met people of all kinds and lived in the biggest cities, small towns and everything in
-                        between. That being said, I am excitedly building the foundation for a career for myself. I've earned my Bachelor's of Computer Science
-                        of which I graduated early cum laude from Kalamazoo College as well as worked in compression algorithm development research to both learn
-                        and prove my aptitude. Feel free to reach out if you'd like to chat.
+                    <div className={this.state.highlights ? 'fadeIn' : 'fadeOut'}>
+                        <p> I'm an aspiring software engineer with the credentials to back up that dream. I grew up in Los Angeles where I took an interest in
+                            the life sciences, working as a zookeeper and frequently volunteering for conservation projects. Since 2018 I have traveled
+                            around the country and gone to college. I've met people of all kinds and lived in the biggest cities, small towns and everything in
+                            between. Feeling well-rounded, I am excitedly building the foundation for a career for myself. I've earned my Bachelor's of Computer Science
+                            of which I graduated early cum laude from Kalamazoo College as well as worked in compression algorithm development research to both learn more
+                            and prove my aptitude.
                         </p>
+                        <button onClick={this.handleFade}>The Highlights</button>
+                    </div>
+                    <div className={this.state.highlights ? 'fadeOut' : 'fadeIn'}>
+                        <p> - Software Engineer
+                            - Data Scientist
+                            - Biotech
+                        </p>
+                        <button onClick={this.handleFade}>My Story</button>
+                    </div>
                 </div>
             </div>
         );
