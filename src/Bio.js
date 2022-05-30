e62112db3aea54e0eb0851474f6e3b35c17dda24
Ronan Wolfe <ronangwolfe@gmail.com>
2022-05-29 19:14:13 -0700

    Updated Styling of About Section

--- src/Bio.js
+++ src/Bio.js
@@ -34,31 +34,41 @@ class Bio extends Component {
     render() {
         return (
             <div className="Bio">
-                <h1>About Me</h1>
                 <div className="Bio-content">
-                    <div>
-                        <h3>Resume & Links</h3>
-                        <button className="DLLogo" onClick={this.DownloadResume}></button>
-                        <button className="GHLogo" onClick={this.Redirect.bind(this, "https://github.com/RonanGW")}></button>
-                        <button className="LILogo" onClick={this.Redirect.bind(this, "https://www.linkedin.com/in/ronan-wolfe/")}></button>
+                    <div className="Bio-item">
+                        <h2>About Me</h2>
+                        <h5>Name:</h5>
+                        <p>Ronan Wolfe</p>
+                        <h5>Area:</h5>
+                        <p>San Fracisco, Bay Area, CA</p>
+                        <h5>Links:</h5>
+                        <button title="Download my Resume" className="DLLogo" onClick={this.DownloadResume}></button>
+                        <button title="Link to my GitHub" className="GHLogo" onClick={this.Redirect.bind(this, "https://github.com/RonanGW")}></button>
+                        <button title="Link to my LinkedIn" className="LILogo" onClick={this.Redirect.bind(this, "https://www.linkedin.com/in/ronan-wolfe/")}></button>
                     </div>
-                    <img src="pfp.jpg" className="Bio-PFP" />
-                    <div className={this.state.highlights ? 'fadeIn' : 'fadeOut'}>
-                        <p> I'm an aspiring software engineer with the credentials to back up that dream. I grew up in Los Angeles where I took an interest in
-                            the life sciences, working as a zookeeper and frequently volunteering for conservation projects. Since 2018 I have traveled
-                            around the country and gone to college. I've met people of all kinds and lived in the biggest cities, small towns and everything in
-                            between. Feeling well-rounded, I am excitedly building the foundation for a career for myself. I've earned my Bachelor's of Computer Science
-                            of which I graduated early cum laude from Kalamazoo College as well as worked in compression algorithm development research to both learn more
-                            and prove my aptitude.
-                        </p>
-                        <button onClick={this.handleFade}>The Highlights</button>
+                    <div className="Bio-item">
+                        <img src="pfp.jpg" className="Bio-PFP" />
                     </div>
-                    <div className={this.state.highlights ? 'fadeOut' : 'fadeIn'}>
-                        <p> - Software Engineer
-                            - Data Scientist
-                            - Biotech
-                        </p>
-                        <button onClick={this.handleFade}>My Story</button>
+                    <div className="Bio-item">
+                        <div className={this.state.highlights ? 'fadeIn' : 'fadeOut'}>
+                            <h3>My Story</h3>
+                            <p> I'm an aspiring software engineer with the credentials to back up that dream. I grew up in Los Angeles where I took an interest in
+                                the life sciences, working as a zookeeper and frequently volunteering for conservation projects. Since 2018 I have traveled
+                                around the country and gone to college. I've met people of all kinds and lived in the biggest cities, small towns and everything in
+                                between. Feeling well-rounded, I am excitedly building the foundation for a career for myself. I've earned my Bachelor's of Computer Science
+                                of which I graduated early cum laude from Kalamazoo College as well as worked in compression algorithm development research to both learn more
+                                and prove my aptitude.
+                            </p>
+                            <button onClick={this.handleFade}>The Highlights</button>
+                        </div>
+                        <div className={this.state.highlights ? 'fadeOut' : 'fadeIn'}>
+                            <h3>The Highlights</h3>
+                            <p> - Software Engineer
+                                - Data Scientist
+                                - Biotech
+                            </p>
+                            <button onClick={this.handleFade}>My Story</button>
+                        </div>
                     </div>
                 </div>
             </div>
