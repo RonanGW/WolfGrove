0d4365a4652521728878e5a9c4eed3d6788a6d62
Ronan Wolfe <ronangwolfe@gmail.com>
2022-06-06 09:17:08 -0700

    Added first testimonials quote

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -6,10 +6,13 @@ class Testimonials extends Component {
         return (
             <div className="Testimonials">
                 <h2>Testimonials</h2>
-                <p>Quote to be acquired. Format Below</p>
-                {/*Quote Icon*/}
-                <p>Lorem ipsum</p>
-                <p>-First, Last, Title</p>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <img style={{ width: "32px", height: "32px" }} src="https://www.pngall.com/wp-content/uploads/4/Quotation-Symbol-PNG.png" />
+                    <p>[Ronan] is willing to think analytically and
+                        creatively about problems and their possible solutions while being tremendously disciplined and
+                        attentive to his work.</p>
+                </div>
+                <p>-Dr. Sandino Vargas-Perez, Professor of Computer Science</p>
             </div>
         );
     }
