40bf4e16418bcbb91f0c4d1a57f40e6e2ac68639
Ronan Wolfe <ronangwolfe@gmail.com>
2022-06-20 17:31:48 -0700

    Finished Portfolio layout changes

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -48,13 +48,13 @@ class Portfolio extends Component {
             var projects = this.props.data.projects.map(function (projects) {
                 var url = "location.href = '" + projects.url + "';"
                 return <div style={{padding:"1%"}}>
-                    <div style={{ boxShadow: "rgba(0, 0, 0, 0.12) 0 5px 5px", backgroundColor: "white", borderTopLeftRadius: "2%", borderTopRightRadius: "2%", width: "30vw", height: "30vh", minWidth: "300px", minHeight: "400px" }}>
+                    <div style={{ boxShadow: "rgba(0, 0, 0, 0.12) 0 5px 5px", backgroundColor: "white", borderTopLeftRadius: "2%", borderTopRightRadius: "2%", width: "25vw", height: "30vh", minWidth: "30px", minHeight: "400px" }}>
                         <img src={projects.image} style={{ borderTopLeftRadius: "2%", borderTopRightRadius: "2%", width: "100%", height: "60%" }} />
                         <div>
                             <p style={{ paddingLeft: "2%", paddingRight: "2%", height: "10%", display: "flex", justifyContent: "center" }}>{projects.description}</p>
                             <div style={{ paddingLeft: "5%", paddingRight: "5%", height: "10%", display: "flex", justifyContent: "space-between" }} >
-                                <button onClick={event => window.location.href = projects.url} >View Github</button>
-                                <h2>{projects.title}</h2>
+                                <button className="GHLogo" onClick={event => window.location.href = projects.url} ></button>
+                                <h2><u>{projects.title}</u></h2>
                             </div>
                         </div>
                     </div>
@@ -70,74 +70,74 @@ class Portfolio extends Component {
                         <div className="HorizontalLine" />
                     </div>
                 </div>
-                <div className="Portfolio-Content">
-                    {/* Project 1*/}
-                    <div className="Project-Button-Container" >
-                        <button style={{ background: "url('https://media.istockphoto.com/photos/sunlight-through-the-coniferous-forest-picture-id148973567?k=20&m=148973567&s=612x612&w=0&h=MqB824yiU7JJHI2JV5AZPW5FW5IAyG5hg4mRd9NuLmA=')" }} className="Project-Button" onClick={this.Expand.bind(this, this.state.ID)}>WolfGrove</button>
-                        {/* Project Expansion box */}
-                        <div className={this.state.projects[0] ? 'openButton' : 'closeButton'}>
-                            <div className="buttonDesc" >
-                                <h3>WolfGrove</h3>
-                                <p>This is my personal website! The place you're at right now! It's a catch all place for
-                                    everything me, all coded by myself. It's designed using react and is meant to both be
-                                    a reference and a display of my ability. The code is all available on Github to view.
-                                    Link Below.
-                                </p>
-                                <button title="Link to my GitHub" className="GHLogo" onClick={this.Redirect.bind(this, "https://github.com/RonanGW")}></button>
-                            </div>
-                        </div>    
-                    </div>
-                    {/* Project 2*/}
-                    <div className="Project-Button-Container" >
-                        <button style={{ background: "url('https://videohive.img.customer.envatousercontent.com/files/2f4e450e-c307-48e7-810b-d61ec2367de5/inline_image_preview.jpg?auto=compress%2Cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=4e2f0efc76db407633c1a8db14a0b94c')" }} className="Project-Button" onClick={this.Expand.bind(this, this.state.ID + 1)}>PhyNGSD</button>
-                        {/* Project Expansion box */}
-                        <div className={this.state.projects[1] ? 'openButton' : 'closeButton'}>
-                            <div className="buttonDesc" >
-                                <h3>PhyNGSD</h3>
-                                <p>This is the project my software development internship was focused on. It is a highly
-                                    scalable parallel compression algorithm for Omic Big Data. It is primarily coded with
-                                    C++, but also OpenMP, MPI & CUDA as well as multithreading and supercomputing. The
-                                    Github link to the decompression algorithm (the section I mainly worked with) is below
+                {/*<div classname="portfolio-content">*/}
+                {/*    */}{/* project 1*/}
+                {/*    <div classname="project-button-container" >*/}
+                {/*        <button style={{ background: "url('https://media.istockphoto.com/photos/sunlight-through-the-coniferous-forest-picture-id148973567?k=20&m=148973567&s=612x612&w=0&h=mqb824yiu7jjhi2jv5azpw5fw5iayg5hg4mrd9nulma=')" }} classname="project-button" onclick={this.expand.bind(this, this.state.id)}>wolfgrove</button>*/}
+                {/*        */}{/* project expansion box */}
+                {/*        <div classname={this.state.projects[0] ? 'openbutton' : 'closebutton'}>*/}
+                {/*            <div classname="buttondesc" >*/}
+                {/*                <h3>wolfgrove</h3>*/}
+                {/*                <p>this is my personal website! the place you're at right now! it's a catch all place for*/}
+                {/*                    everything me, all coded by myself. it's designed using react and is meant to both be*/}
+                {/*                    a reference and a display of my ability. the code is all available on github to view.*/}
+                {/*                    link below.*/}
+                {/*                </p>*/}
+                {/*                <button title="link to my github" classname="ghlogo" onclick={this.redirect.bind(this, "https://github.com/ronangw")}></button>*/}
+                {/*            </div>*/}
+                {/*        </div>    */}
+                {/*    </div>*/}
+                {/*    */}{/* project 2*/}
+                {/*    <div classname="project-button-container" >*/}
+                {/*        <button style={{ background: "url('https://videohive.img.customer.envatousercontent.com/files/2f4e450e-c307-48e7-810b-d61ec2367de5/inline_image_preview.jpg?auto=compress%2cformat&fit=crop&crop=top&max-h=8000&max-w=590&s=4e2f0efc76db407633c1a8db14a0b94c')" }} classname="project-button" onclick={this.expand.bind(this, this.state.id + 1)}>phyngsd</button>*/}
+                {/*        */}{/* project expansion box */}
+                {/*        <div classname={this.state.projects[1] ? 'openbutton' : 'closebutton'}>*/}
+                {/*            <div classname="buttondesc" >*/}
+                {/*                <h3>phyngsd</h3>*/}
+                {/*                <p>this is the project my software development internship was focused on. it is a highly*/}
+                {/*                    scalable parallel compression algorithm for omic big data. it is primarily coded with*/}
+                {/*                    c++, but also openmp, mpi & cuda as well as multithreading and supercomputing. the*/}
+                {/*                    github link to the decompression algorithm (the section i mainly worked with) is below*/}
 
-                                </p>
-                                <button title="Link to my GitHub" className="GHLogo" onClick={this.Redirect.bind(this, "https://github.com/pcdslab/PHYNGSD")}></button>
-                            </div>
-                        </div>    
-                    </div>
-                    {/* Project 3*/}
-                    <div className="Project-Button-Container" >
-                        <button style={{ background: "url('https://media.istockphoto.com/photos/green-forest-picture-id1173733640?b=1&k=20&m=1173733640&s=170667a&w=0&h=qVsBTxqYFPqZZkRr1OzHu-92jNtscIrST0UZin26CQU=')" }} className="Project-Button" onClick={this.Expand.bind(this, this.state.ID + 2)}>Bio-Loss</button>
-                        {/* Project Expansion box */}
-                        <div className={this.state.projects[2] ? 'openButton' : 'closeButton'}>
-                            <div className="buttonDesc" >
-                                <h3>Bio-Loss</h3>
-                                <p>This was a project I started as a final for one of my senior year courses. This project
-                                    was meant to be an proof of concept display for an interactive educational webpage for any audience
-                                    that quantifies Biodiversity loss per country as a way of explaining how severe the issue is. 
-                                    This project involved guiding several novice to creates the different aspects of the project.
-                                    I intend to improve the code, but the current code is available below.
-                                </p>
-                                <button title="Link to my GitHub" className="GHLogo" onClick={this.Redirect.bind(this, "https://github.com/langstonhowley/seminar-webpage")}></button>
-                            </div>
-                        </div>    
-                    </div>
-                    {/* Project 4*/}
-                    <div className="Project-Button-Container" >
-                        <button className="Project-Button" style={{ backgroundColor: "#DFEAE2", color: "#18191A" }} onClick={this.Expand.bind(this, this.state.ID + 3)}>TIPO</button>
-                        {/* Project Expansion box */}
-                        <div className={this.state.projects[3] ? 'openButton' : 'closeButton'}>
-                            <div className="buttonDesc" >
-                                <h3>TIPO</h3>
-                                <p>This is a project I started with a friend as a way to introduce programming to her.
-                                    It's meant to be a simple local project manager/to-do list. The source code is viewable
-                                    at the link below.
-                                </p>
-                                <button title="Link to my GitHub" className="GHLogo" onClick={this.Redirect.bind(this, "https://github.com/GenesisMM/TIPO")}></button>
-                            </div>
-                        </div>    
-                    </div>
-                </div>
-                <div style={{ display: "flex", flexDirection: "row", flexFlow: "row wrap" }}>
+                {/*                </p>*/}
+                {/*                <button title="link to my github" classname="ghlogo" onclick={this.redirect.bind(this, "https://github.com/pcdslab/phyngsd")}></button>*/}
+                {/*            </div>*/}
+                {/*        </div>    */}
+                {/*    </div>*/}
+                {/*    */}{/* project 3*/}
+                {/*    <div classname="project-button-container" >*/}
+                {/*        <button style={{ background: "url('https://media.istockphoto.com/photos/green-forest-picture-id1173733640?b=1&k=20&m=1173733640&s=170667a&w=0&h=qvsbtxqyfpqzzkrr1ozhu-92jntscirst0uzin26cqu=')" }} classname="project-button" onclick={this.expand.bind(this, this.state.id + 2)}>bio-loss</button>*/}
+                {/*        */}{/* project expansion box */}
+                {/*        <div classname={this.state.projects[2] ? 'openbutton' : 'closebutton'}>*/}
+                {/*            <div classname="buttondesc" >*/}
+                {/*                <h3>bio-loss</h3>*/}
+                {/*                <p>this was a project i started as a final for one of my senior year courses. this project*/}
+                {/*                    was meant to be an proof of concept display for an interactive educational webpage for any audience*/}
+                {/*                    that quantifies biodiversity loss per country as a way of explaining how severe the issue is. */}
+                {/*                    this project involved guiding several novice to creates the different aspects of the project.*/}
+                {/*                    i intend to improve the code, but the current code is available below.*/}
+                {/*                </p>*/}
+                {/*                <button title="link to my github" classname="ghlogo" onclick={this.redirect.bind(this, "https://github.com/langstonhowley/seminar-webpage")}></button>*/}
+                {/*            </div>*/}
+                {/*        </div>    */}
+                {/*    </div>*/}
+                {/*    */}{/* project 4*/}
+                {/*    <div classname="project-button-container" >*/}
+                {/*        <button classname="project-button" style={{ backgroundcolor: "#dfeae2", color: "#18191a" }} onclick={this.expand.bind(this, this.state.id + 3)}>tipo</button>*/}
+                {/*        */}{/* project expansion box */}
+                {/*        <div classname={this.state.projects[3] ? 'openbutton' : 'closebutton'}>*/}
+                {/*            <div classname="buttondesc" >*/}
+                {/*                <h3>tipo</h3>*/}
+                {/*                <p>this is a project i started with a friend as a way to introduce programming to her.*/}
+                {/*                    it's meant to be a simple local project manager/to-do list. the source code is viewable*/}
+                {/*                    at the link below.*/}
+                {/*                </p>*/}
+                {/*                <button title="link to my github" classname="ghlogo" onclick={this.redirect.bind(this, "https://github.com/genesismm/tipo")}></button>*/}
+                {/*            </div>*/}
+                {/*        </div>    */}
+                {/*    </div>*/}
+                {/*</div>*/}
+                <div style={{ paddingTop: "3%", display: "flex", flexDirection: "row", flexFlow: "row wrap", justifyContent: "center" }}>
                     {projects}
                 </div>
             </div>
