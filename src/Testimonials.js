d4707ad6ff7b8906df6875569786a9127b31b3cb
Ronan Wolfe <ronangwolfe@gmail.com>
2022-06-27 12:13:12 -0700

    Refactored Testimonials css

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -7,15 +7,15 @@ class Testimonials extends Component {
             <div className="Testimonials">
                 <div className="Section-Header">
                     <div className="Section-Header-Alignment">
-                        <h1 style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                        <h1 className="Section-Title-Line">
                             <div className="SectionNameLine" />
                             Testimonials
                             <div className="SectionNameLine" />
                         </h1>
                     </div>
                 </div>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                    <img style={{ width: "32px", height: "32px" }} src="https://www.pngall.com/wp-content/uploads/4/Quotation-Symbol-PNG.png" />
+                <div className="TestimonialsContent">
+                    <img className="QuoteImg" src="https://www.pngall.com/wp-content/uploads/4/Quotation-Symbol-PNG.png" />
                     <p>[Ronan] is willing to think analytically and
                         creatively about problems and their possible solutions while being tremendously disciplined and
                         attentive to his work.</p>
