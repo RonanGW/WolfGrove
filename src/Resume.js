d5af5a60eb5717b527ed4945a2834b20992f48e9
Ronan Wolfe <ronangwolfe@gmail.com>
2022-06-27 11:10:19 -0700

    Refactored Job css and collected job header section together.

--- src/Resume.js
+++ src/Resume.js
@@ -20,32 +20,7 @@ class Resume extends Component {
                             <p>{desc}</p>
                         </div>
                     </div>
-                    {/*<div className="SchoolDesc">*/}
-                    {/*    */}{/* Left side details */}
-                    {/*    <div className="TitlePhrase">*/}
-                    {/*        <div className="LogoPhrase">*/}
-                    {/*        <img style={{ width: "64px", height: "64px" }} src={education.icon} />*/}
-                    {/*        <h2>{education.school}</h2>*/}
-                    {/*        </div>*/}
-                    {/*        <h3>{education.degree}</h3>*/}
-                    {/*        <div className="LogoPhrase">*/}
-                    {/*            <img style={{ width: "20px", height: "20px" }} src="./Calendar.png" />*/}
-                    {/*            <p>{date}</p>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="LogoPhrase">*/}
-                    {/*            <img style={{ width: "16px", height: "20px" }} src="./LocPin.png" />*/}
-                    {/*        <p>{education.location}</p>*/}
-                    {/*        </div>*/}
-                    {/*        </div>*/}
-                    {/*        */}{/* Divider */}
-                    {/*        <div className="VerticalLine" />*/}
-                    {/*        */}{/* Right side description */}
-                    {/*        <div>*/}
-                    {/*            <p className="DescPhrase">*/}
-                    {/*               {desc}*/}
-                    {/*            </p>*/}
-                    {/*        </div>*/}
-                        </div>
+                </div>
             })
             var jobs = this.props.data.work.map(function (work) {
                 var date = work.startDate + '-\n' + work.endDate;
@@ -54,43 +29,18 @@ class Resume extends Component {
                 var tasks = work.tasks.map(function (task) {
                     return <li>{task}</li>
                 });
-                return <div style={{ paddingLeft: "10%", paddingRight: "10%", display: "flex", flexDirection: "row" }}>
-                        <img style={{ borderRadius: "10%", width: "10%", height: "10%" }} src={work.icon} />
-                        <div style={{ paddingLeft: "5%" }}>
-                            <h2>{work.title}</h2>
-                            <h3>{work.company}</h3>
-                            <p>{work.location}</p>
-                            <p><i>{date}</i></p>
-                            <div >
+                return <div className="JobDesc">
+                        <img className="CompanyIcon" src={work.icon} />
+                        <div className="JobContainer">
+                            <h2 className="JobTitle">{work.title}</h2>
+                            <h3 className="JobCompany">{work.company}</h3>
+                            <p className="JobItem">{work.location}</p>
+                            <p className="JobItem"><i>{date}</i></p>
+                            <div>
                                 <p>{desc}</p>
                                 <ul>{tasks}</ul>
                             </div>
                         </div>
-                    {/*<div className="JobDesc">*/}
-                    {/*    */}{/* Left side details */}
-                    {/*    <div className="TitlePhrase">*/}
-                    {/*        <h2>{work.title}</h2>*/}
-                    {/*        <div className="LogoPhrase">*/}
-                    {/*            <img style={{ width: "32px", height: "32px" }} src={work.icon} />*/}
-                    {/*            <h3>{work.company}</h3>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="LogoPhrase">*/}
-                    {/*            <img style={{ width: "20px", height: "20px" }} src="./Calendar.png" />*/}
-                    {/*            <p>{date}</p>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="LogoPhrase">*/}
-                    {/*            <img style={{ width: "16px", height: "20px" }} src="./LocPin.png" />*/}
-                    {/*            <p>{work.location}</p>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*    */}{/* Divider */}
-                    {/*    <div className="VerticalLine" />*/}
-                    {/*    */}{/* Right side description */}
-                    {/*    <div className="DescPhrase">*/}
-                    {/*        <p>{desc}</p>*/}
-                    {/*        <ul>{tasks}</ul>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                     </div>
             })
         }
@@ -100,7 +50,7 @@ class Resume extends Component {
             <div className="Resume-container">
                 <div className="Section-Header">
                     <div className="Section-Header-Alignment">
-                            <h1 style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                            <h1 className="Section-Title-Line">
                                 <div className="SectionNameLine" />
                                 Experience
                                 <div className="SectionNameLine" />
@@ -111,13 +61,7 @@ class Resume extends Component {
                     {/* Work Experience Section */}
                     <div className="Resume-section">
                         <div>
-                            {/* Section header */}
-                            {/*<h1>Experience</h1>*/}
-                            {/*<div style={{ display: "flex", flexDirection: "row" }}>*/}
-                            {/*    */}{/* Styled item bullet points*/}
-                            {/*    <img src="./LeafWalls.png" />*/}
-                                <div>{jobs}</div>
-                            {/*</div>*/}
+                            <div>{jobs}</div>
                         </div>
                     </div>
                     {/* Education section */}
@@ -128,7 +72,7 @@ class Resume extends Component {
                             {/*<div style={{ display: "flex", flexDirection: "row" }}>*/}
                             {/*    */}{/* Styled item bullet points*/}
                             {/*    <img src="./LeafWalls.png" />*/}
-                                <div style={{ display: "flex", flexDirection: "column" }}>{education}</div>
+                            <div>{education}</div>
                             {/*</div>*/}
                         </div>
                     </div>
